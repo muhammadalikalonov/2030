8ceb0e6d3915050661c8d7da60699ca71bd147a5
muhammadalikalonov <59058305+muhammadalikalonov@users.noreply.github.com>
2022-01-10 15:37:44 +0500

    Delete PhpstormProjects/2030.serio.uz directory

--- PhpstormProjects/2030.serio.uz/wp-content/themes/serio-child/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-add_action( 'wp_enqueue_scripts', 'pearl_child_enqueue_parent_styles' );
-
-function pearl_child_enqueue_parent_styles() {
-	wp_enqueue_style( 'child-style', get_stylesheet_uri());
-}
